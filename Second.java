f23c7a963582203b46491bc53d2030c9ebe6f512
Prince <155230985+princef1@users.noreply.github.com>
2024-02-15 19:47:47 +0545

    Create Second.java

---
+++ Second.java
@@ -0,0 +1,13 @@
+public class Second{
+    public static void main(String[] args) {
+        String a="Hello";
+        String b="Prince";
+        //Swapping values
+        String temp=null; //temp=temporary however we can use anything instead of temp
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+    }
+}
